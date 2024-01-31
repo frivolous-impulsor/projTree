fa4909960f206cdb5b2a409d4f94ff923d27c995
frivolous-impulsor <ultrapilot95@gmail.com>
2024-01-30 21:43:10 -0500

    base template restyling to mimic duolingo

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from post import views as post_views
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
