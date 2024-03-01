5f66f41f70421fd54f30087e3c567c91600b8349
frivolous-impulsor <ultrapilot95@gmail.com>
2024-03-01 09:23:15 -0500

    test sync on pyanywhere

--- projTree/settings.py
+++ projTree/settings.py
@@ -138,6 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = 'seed_list'
